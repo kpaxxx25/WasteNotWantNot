c8fc918c84303191be37a4b494d90a7331bac410
Alec Thomas <acelestialknight@outlook.com>
2021-04-22 16:25:29 -0400

    attempted migration fix; needs update method

--- Migrations/20210421173802_IngredientsMigration.Designer.cs
+++ Migrations/20210421173802_IngredientsMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WNWN.Data;
 
 namespace WNWN.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210421173802_IngredientsMigration")]
+    partial class IngredientsMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210421173802_IngredientsMigration.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WNWN.Migrations
+{
+    public partial class IngredientsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactEmail",
+                table: "Ingredients");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Weight",
+                table: "Ingredients",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpirationDate",
+                table: "Ingredients",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Unit",
+                table: "Ingredients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Ingredients",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Ingredients");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Weight",
+                table: "Ingredients",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExpirationDate",
+                table: "Ingredients",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactEmail",
+                table: "Ingredients",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
